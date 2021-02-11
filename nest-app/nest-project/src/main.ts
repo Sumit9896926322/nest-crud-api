81dcb12b37b49bd5e2d94a9b1150e51dfd08e242
Sumit singh <sumit@bitcs.in>
2021-02-11 16:28:37 +0530

    logger message in bootstrap

--- nest-app/nest-project/src/main.ts
+++ nest-app/nest-project/src/main.ts
@@ -1,8 +1,13 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger("bootstrap");
+  const port = 3000;
   await app.listen(3000);
+
+  logger.log(`App running on port ${port}`);
 }
 bootstrap();
